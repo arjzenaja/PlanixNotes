7e8c4d4a7a8b8167ba54dd35a75aa14b40ecac99
arjzenaja <arjzenimato1706@gmail.com>
2024-12-05 10:53:24 +0700

    final commit

---
+++ app/src/main/java/com/example/thenotesapp/Login.kt
@@ -0,0 +1,35 @@
+package com.example.thenotesapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.thenotesapp.databinding.ActivityLoginBinding
+
+class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        val pindah = findViewById<Button>(R.id.login)
+        pindah.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        val pindah2 = findViewById<TextView>(R.id.backRegister)
+        pindah2.setOnClickListener{
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
